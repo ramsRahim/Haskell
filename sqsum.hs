7c206caace5c96d43c3ca1509f1e76ccefe23b26
ramsRahim <rahimhossain23@gmail.com>
2023-11-19 18:41:40 -0500

    square sum implemented

---
+++ sqsum.hs
@@ -0,0 +1,11 @@
+-- FILEPATH: /home/rhossain/exp/Haskell/sqsum.hs
+
+-- Define the sqsum function
+sqsum :: Num a => [a] -> a
+sqsum xs = foldr (+) 0 (map (^2) xs)
+
+-- Define the main module
+main :: IO ()
+main = do
+  let values = [1, 3, 5]
+  putStrLn $ "Sum of squares: " ++ show (sqsum values)
